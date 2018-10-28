56f9dda11e929059f107b16e8c3c6dcbe23d905d
Lorenzo Sanesi <lorenzo.sanesi.dev@gmail.com>
2018-10-28 15:14:17 +0100

    Fix service provider

--- src/Prettus/Repository/Providers/RepositoryServiceProvider.php
+++ src/Prettus/Repository/Providers/RepositoryServiceProvider.php
@@ -45,7 +45,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->commands('Prettus\Repository\Generators\Commands\RepositoryCommand');
         $this->commands('Prettus\Repository\Generators\Commands\TransformerCommand');
         $this->commands('Prettus\Repository\Generators\Commands\PresenterCommand');
-        $this->commands('Prettus\Repository\Generators\Commands\BDCommand');
+        $this->commands('Prettus\Repository\Generators\Commands\DBCommand');
         $this->commands('Prettus\Repository\Generators\Commands\EntityCommand');
         $this->commands('Prettus\Repository\Generators\Commands\ValidatorCommand');
         $this->commands('Prettus\Repository\Generators\Commands\ControllerCommand');
